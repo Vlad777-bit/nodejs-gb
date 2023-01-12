afe0edb6b69cea7782e1d1b7b6ebeb6d2d58498a
Vladislav Nozhkin <mega.nozhkin@pm.me>
2023-01-12 10:56:48 +0300

    lesson-3 is ready

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,3 @@
-import EventEmitter from 'events';
-import { Handler } from './lesson-2/Handler';
-import { Time } from './lesson-2/Time';
+import { lesson3 } from './lesson-3/index';
 
-class TimeEmitter extends EventEmitter {}
-const emitter = new TimeEmitter();
-
-emitter.on('getTimers', Handler.handler.bind(Handler));
-
-Handler.interval = setInterval(() => emitter.emit('getTimers'), 1000);
-
-const args = process.argv.slice(2);
-
-args.forEach((item) => {
-	Handler.setTimer(new Time(item));
-});
+lesson3();
